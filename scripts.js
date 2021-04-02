c5505ecd15a0e6835fd92d45f171e4864330f994
Laura Tissi <laura.tracierra@gmail.com>
2021-04-01 23:13:40 -0300

    início do js com difuculdade, amanhã tem que continuar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,31 @@
-function item_selecionado() {
-    alert ("item selecionado");
-	let elemento = document.querySelector(".item");
-    elemento.classList.add("bordaverde");
+function item_selecionado_prato(itemselecionado) {
+    if (itemselecionado != null){
+        alert ("item selecionado");
+        const prato_excluido = document.querySelector(".item, .steak_au_gratin")
+        prato_excluido.classList.remove("bordaverde");
+    }
+    const prato = document.querySelector(itemselecionado)
+    prato.classList.add("bordaverde");
+}
+
+function item_selecionado_bebida(itemselecionado) {
+    if (itemselecionado != null){
+        alert ("item selecionado");
+        const prato_excluido = document.querySelector(".item, .steak_au_gratin")
+        prato_excluido.classList.remove("bordaverde");
+    }
+    const prato = document.querySelector(itemselecionado)
+    prato.classList.add("bordaverde");
+}
+
+function item_selecionado_sobremesa(itemselecionado) {
+    if (itemselecionado != null){
+        alert ("item selecionado");
+        const prato_excluido = document.querySelector(".item, .steak_au_gratin")
+        prato_excluido.classList.remove("bordaverde");
+    }
+    const prato = document.querySelector(itemselecionado)
+    prato.classList.add("bordaverde");
 }
 
 function fechar_pedido() {
